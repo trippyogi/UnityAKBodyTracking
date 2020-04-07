32b322c800c883500b58b7141076cf0cb43cb8ab
Jeremy <peace@trippyogi.com>
2020-04-06 23:18:48 -0700

    Cleanup SkeletalTrackingProvider

--- Assets/Scripts/SkeletalTrackingProvider.cs
+++ Assets/Scripts/SkeletalTrackingProvider.cs
@@ -1,30 +1,22 @@
 ﻿using System;
-using System.Threading.Tasks;
-using System.IO;
-using System.Runtime.InteropServices;
 using Microsoft.Azure.Kinect.Sensor;
 using Microsoft.Azure.Kinect.BodyTracking;
 using UnityEngine;
 
 public class SkeletalTrackingProvider : BackgroundDataProvider
 {
-    bool readFirstFrame = false;
-    TimeSpan initialTimestamp;
-
-    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter { get; set; } = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-
-    public Stream RawDataLoggingFile = null;
+    private bool _readFirstFrame = false;
 
     protected override void RunBackgroundThreadAsync(int id)
     {
         try
         {
-            UnityEngine.Debug.Log("Starting body tracker background thread.");
+            Debug.Log("Starting body tracker background thread.");
 
             // Buffer allocations.
-            BackgroundData currentFrameData = new BackgroundData();
+            var currentFrameData = new BackgroundData();
             // Open device.
-            using(Device device = Device.Open(id))
+            using (var device = Device.Open(id))
             {
                 device.StartCameras(new DeviceConfiguration()
                 {
@@ -34,27 +26,29 @@ public class SkeletalTrackingProvider : BackgroundDataProvider
                     WiredSyncMode = WiredSyncMode.Standalone,
                 });
 
-                UnityEngine.Debug.Log("Open K4A device successful. id " + id + "sn:" + device.SerialNum);
+                Debug.Log("Open K4A device successful. id " + id + "sn:" + device.SerialNum);
 
                 var deviceCalibration = device.GetCalibration();
 
-                using(Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default }))
+                using (var tracker = Tracker.Create(deviceCalibration,
+                    new TrackerConfiguration()
+                        {ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default}))
                 {
-                    UnityEngine.Debug.Log("Body tracker created.");
-                    while(m_runBackgroundThread)
+                    Debug.Log("Body tracker created.");
+                    while (m_runBackgroundThread)
                     {
-                        using(Capture sensorCapture = device.GetCapture())
+                        using (var sensorCapture = device.GetCapture())
                         {
                             // Queue latest frame from the sensor.
                             tracker.EnqueueCapture(sensorCapture);
                         }
 
                         // Try getting latest tracker frame.
-                        using(Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false))
+                        using (var frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false))
                         {
-                            if(frame == null)
+                            if (frame == null)
                             {
-                                UnityEngine.Debug.Log("Pop result from tracker timeout!");
+                                Debug.Log("Pop result from tracker timeout!");
                             }
                             else
                             {
@@ -63,46 +57,32 @@ public class SkeletalTrackingProvider : BackgroundDataProvider
                                 currentFrameData.NumOfBodies = frame.NumberOfBodies;
 
                                 // Copy bodies.
-                                for(uint i = 0; i < currentFrameData.NumOfBodies; i++)
+                                for (uint i = 0; i < currentFrameData.NumOfBodies; i++)
                                 {
-                                    currentFrameData.Bodies[i].CopyFromBodyTrackingSdk(frame.GetBody(i), deviceCalibration);
+                                    currentFrameData.Bodies[i]
+                                        .CopyFromBodyTrackingSdk(frame.GetBody(i), deviceCalibration);
                                 }
 
-                                // Store depth image.
-                                Capture bodyFrameCapture = frame.Capture;
-                                Image depthImage = bodyFrameCapture.Depth;
-                                if(!readFirstFrame)
+                                if (!_readFirstFrame)
                                 {
-                                    readFirstFrame = true;
-                                    initialTimestamp = depthImage.DeviceTimestamp;
-                                }
-
-                                // Read image data from the SDK.
-                                var depthFrame = MemoryMarshal.Cast<byte, ushort>(depthImage.Memory.Span);
-
-                                if(RawDataLoggingFile != null && RawDataLoggingFile.CanWrite)
-                                {
-                                    binaryFormatter.Serialize(RawDataLoggingFile, currentFrameData);
+                                    _readFirstFrame = true;
                                 }
 
                                 // Update data variable that is being read in the UI thread.
                                 SetCurrentFrameData(ref currentFrameData);
                             }
-
                         }
                     }
+
                     tracker.Dispose();
                 }
+
                 device.Dispose();
             }
-            if(RawDataLoggingFile != null)
-            {
-                RawDataLoggingFile.Close();
-            }
         }
-        catch(Exception e)
+        catch (Exception e)
         {
-            UnityEngine.Debug.LogError(e.Message);
+            Debug.LogError(e.Message);
         }
     }
 }
