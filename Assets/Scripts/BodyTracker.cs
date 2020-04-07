6bd69b4d566629d469ea846044426f91affcadcf
Jeremy <peace@trippyogi.com>
2020-04-06 23:26:14 -0700

    Cleanup TrackerHandler and main

---
+++ Assets/Scripts/BodyTracker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+public class BodyTracker : MonoBehaviour
+{
+    // Handler for SkeletalTracking thread.
+    public GameObject Tracker;
+    private BackgroundData _lastFrameData = new BackgroundData();
+    private BackgroundDataProvider _backgroundDataProvider;
+
+    void Start()
+    {
+        var skeletalTrackingProvider = new SkeletalTrackingProvider();
+
+        //tracker ids needed for when there are two trackers
+        const int TRACKER_ID = 0;
+        skeletalTrackingProvider.StartClientThread(TRACKER_ID);
+        _backgroundDataProvider = skeletalTrackingProvider;
+    }
+
+    void Update()
+    {
+        if (!_backgroundDataProvider.IsRunning) return;
+        if (!_backgroundDataProvider.GetCurrentFrameData(ref _lastFrameData)) return;
+        if (_lastFrameData.NumOfBodies != 0)
+        {
+            Tracker.GetComponent<TrackerHandler>().UpdateTracker(_lastFrameData);
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Stop background threads.
+        _backgroundDataProvider?.StopClientThread();
+    }
+}
